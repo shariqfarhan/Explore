1272562c248cae38868df41e2ad89ffa79854e6a
shariqfarhan <57046534+shariqfarhan@users.noreply.github.com>
2023-02-18 08:15:30 +0530

    Create training.py

---
+++ Assignment_8/training.py
@@ -0,0 +1,79 @@
+def train(model, device, train_loader, criterion, scheduler, optimizer, use_l1=False, lambda_l1=0.01):
+    """Function to train the model
+
+    Args:
+        model (instance): torch model instance of defined model
+        device (str): "cpu" or "cuda" device to be used
+        train_loader (instance): Torch Dataloader instance for trainingset
+        criterion (instance): criterion to used for calculating the loss
+        scheduler (function): scheduler to be used
+        optimizer (function): optimizer to be used
+        use_l1 (bool, optional): L1 Regularization method set True to use . Defaults to False.
+        lambda_l1 (float, optional): Regularization parameter of L1. Defaults to 0.01.
+
+    Returns:
+        float: accuracy and loss values
+    """
+    model.train()
+    pbar = tqdm(train_loader)
+    lr_trend = []
+    correct = 0
+    processed = 0
+    train_loss = 0
+
+    for batch_idx, (data, target) in enumerate(pbar):
+        # get samples
+        data, target = data.to(device), target.to(device)
+
+        # Init
+        optimizer.zero_grad()
+        # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch 
+        # accumulates the gradients on subsequent backward passes. Because of this, when you start your training loop, 
+        # ideally you should zero out the gradients so that you do the parameter update correctly.
+
+        # Predict
+        y_pred = model(data)
+        # Calculate loss
+        loss = criterion(y_pred, target)
+
+        l1=0
+        if use_l1:
+            for p in model.parameters():
+                l1 = l1 + p.abs().sum()
+        loss = loss + lambda_l1*l1
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        # updating LR
+        if scheduler:
+            if not isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
+                scheduler.step()
+                lr_trend.append(scheduler.get_last_lr()[0])
+
+        train_loss += loss.item()
+
+        # Update pbar-tqdm
+        pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        processed += len(data)
+        
+
+        pbar.set_description(desc= f'Batch_id={batch_idx} Loss={train_loss/(batch_idx + 1):.5f} Accuracy={100*correct/processed:0.2f}%')
+    return 100*correct/processed, train_loss/(batch_idx + 1), lr_trend
+  
+from torch_lr_finder import LRFinder
+
+
+def find_lr(net, optimizer, criterion, train_loader):
+    """Find learning rate for using One Cyclic LRFinder
+    Args:
+        net (instace): torch instace of defined model
+        optimizer (instance): optimizer to be used
+        criterion (instance): criterion to be used for calculating loss
+        train_loader (instance): torch dataloader instace for trainig set
+    """
+    lr_finder = LRFinder(net, optimizer, criterion, device="cuda")
+    lr_finder.range_test(train_loader, end_lr=10, num_iter=100, step_mode="exp")
+    lr_finder.plot()
+    lr_finder.reset()
