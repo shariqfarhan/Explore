cf86566024653e837dc4f3ca7d9706be19e88f90
shariqfarhan <57046534+shariqfarhan@users.noreply.github.com>
2023-03-08 16:49:40 +0530

    Update model.py

--- Assignment_7/model.py
+++ Assignment_7/model.py
@@ -1,140 +1,89 @@
+'''ResNet in PyTorch.
+
+For Pre-activation ResNet, see 'preact_resnet.py'.
+
+Reference:
+[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+    Deep Residual Learning for Image Recognition. arXiv:1512.03385
+'''
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-dropout_value = 0.0
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.convblock_0 = nn.Sequential(
-                       nn.Conv2d(in_channels=3,out_channels=16,kernel_size=(3,3),dilation=1,stride=1,padding=1,bias=False,),
-                       nn.ReLU(),
-                       nn.BatchNorm2d(16),
-                       nn.Dropout(dropout_value), # Input - 32x32x3 | Output - 32X32X16 | RF=3
 
-                       nn.Conv2d(in_channels=16,out_channels=32,kernel_size=(3,3),dilation=1,stride=1,padding=1,bias=False,),
-                       nn.ReLU(),
-                       nn.BatchNorm2d(32),
-                       nn.Dropout(dropout_value), # Input - 32X32X16 | Output - 32X32x32 |RF=5
+class BasicBlock(nn.Module):
+    expansion = 1
 
-                       nn.Conv2d(in_channels=32,out_channels=32,kernel_size=(3,3),dilation=1,stride=1,padding=1,bias=False,),
-                       nn.ReLU(),
-                       nn.BatchNorm2d(32),
-                       nn.Dropout(dropout_value), # Input - 32X32X32 | Output - 32X32X64 |RF= 7
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
 
-                       nn.Conv2d(in_channels=32,out_channels=32,kernel_size=(3,3),dilation=1,stride=1,padding=1,bias=False,),
-                       nn.ReLU(),
-                       nn.BatchNorm2d(32),
-                       nn.Dropout(dropout_value), # Input - 32X32X64 | Output - 32X32X64 |RF= 9
-                      )
-        
-        # depthwise seperable Convolution 1
-        self.convblock_1 = nn.Sequential(
-        
-                       nn.Conv2d(in_channels=32,out_channels=64,kernel_size=(3,3),stride=(2,2),dilation=1,padding=1,bias=False,),# maxpool added after RF >11
-                       nn.ReLU(),
-                       nn.BatchNorm2d(64),
-                       nn.Dropout(dropout_value), # Input - 32X32X64 | Output - 16X16X64 |RF=11
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
 
-                       nn.Conv2d(in_channels=64,out_channels=64,groups=64,kernel_size=(3,3),stride=(1,1),dilation=1,padding=1,bias=False,),
-                       # Input - 16X16X64 | Output - 16X16X64 | RF=15
-                       nn.Conv2d(in_channels=64,out_channels=128,kernel_size=(1,1),stride=(1,1),padding=0,bias=False,),
-                       # Input - 16X16X64 | Output - 16X16X64 | RF=15
-                       nn.ReLU(),
-                       nn.BatchNorm2d(128), 
-                       nn.Dropout(dropout_value), # 16X16X64 | RF=21                                       
-                       # pointwise   
-
-                       nn.Conv2d(in_channels=128,out_channels=128,groups=128,kernel_size=(3,3),dilation=1,stride=(1,1),padding=1,bias=False,),
-                       # Input - 16X16X64 | Output - 16X16X64 | RF=29
-                       nn.Conv2d(in_channels=128,out_channels=64,kernel_size=(1,1),padding=0,bias=False,),
-                       nn.ReLU(),
-                       nn.BatchNorm2d(64),   
-                       nn.Dropout(dropout_value), 
-                       # Input - 16X16X64 | Output - 16X16X32 | RF=29
-                       
-                      #nn.Conv2d(in_channels=64,out_channels=64,kernel_size=(3,3),stride=(1,1),dilation=2,padding=1,bias=False,),
-                      # #  nn.Conv2d(in_channels=64,out_channels=64,groups=64,kernel_size=(3,3),stride=(1,1),padding=1,bias=False,),
-                      # #  nn.Conv2d(in_channels=64,out_channels=64,kernel_size=(1,1),padding=0,bias=False,),
-                      #  nn.ReLU(),
-                      #  nn.BatchNorm2d(64),   
-                      #  nn.Dropout(dropout_value) , # 16X16X64 | RF=29                                                         
-                       )
-        # depthwise seperable Convolution 2
-        self.convblock_2 = nn.Sequential(
-        
-                       nn.Conv2d(in_channels=64,out_channels=32,kernel_size=(3,3),stride=(2,2),dilation=1,padding=1,bias=False,),
-                       nn.ReLU(),
-                       nn.BatchNorm2d(32),   
-                       nn.Dropout(dropout_value), 
-                      # # Input - 16X16X32 | Output - 8X8X32 | RF=37
-
-                       nn.Conv2d(in_channels=32,out_channels=32,groups=32,kernel_size=(3,3),stride=(1,1),padding=1,bias=False,),
-                       # # Input - 8X8X32 | Output - 8X8X32 | RF=45
-                       nn.Conv2d(in_channels=32,out_channels=64,kernel_size=(1,1),stride=(1,1),padding=0,bias=False,),
-                       # # Input - 8X8X32 | Output - 8X8X64 | RF=45
-                       nn.ReLU(),
-                       nn.BatchNorm2d(64),  
-                       nn.Dropout(dropout_value),
-                      # pointwise   
-
-                       nn.Conv2d(in_channels=64,out_channels=64,groups=64,kernel_size=(3,3),stride=(1,1),padding=1,bias=False,),
-                       # # Input - 8X8X64 | Output - 8X8X128 | RF=53
-                       nn.Conv2d(in_channels=64,out_channels=64,kernel_size=(1,1),stride=(1,1),padding=0,bias=False,),
-                       nn.ReLU(),
-                       nn.BatchNorm2d(64),  # pointwise 
-                       nn.Dropout(dropout_value) 
-                       # # Input - 8X8X64 | Output - 8X8X64 | RF=53
-
-                      )
-        # depthwise seperable Convolution 2
-        self.convblock_3 = nn.Sequential(
-        
-                       #Maxpooling
-                       nn.Conv2d(in_channels=64,out_channels=64,kernel_size=(3,3),dilation=1,stride=(2,2),padding=1,bias=False),
-                       nn.ReLU(),
-                       nn.BatchNorm2d(64),  
-                       nn.Dropout(dropout_value),
-                      # # Input - 8X8X64 | Output - 4X4X64 | RF=69
-
-                       nn.Conv2d(in_channels=64,out_channels=128,groups=64,kernel_size=(3,3),stride=(1,1),padding=1,bias=False,),
-                       # # Input - 4X4X64 | Output - 4X4X128 | RF=85
-                       nn.Conv2d(in_channels=128,out_channels=64,kernel_size=(1,1),stride=(1,1),padding=0,bias=False,),
-                       nn.ReLU(),
-                       nn.BatchNorm2d(64),
-                       nn.Dropout(dropout_value),
-                      #  # Input - 4X4X128 | Output - 4X4X64 | RF=85
-                     
-                       nn.Conv2d(in_channels=64,out_channels=32,kernel_size=(3,3),dilation=2,stride=(1,1),padding=2,bias=False),
-                       nn.ReLU(),
-                       nn.BatchNorm2d(32),  
-                       nn.Dropout(dropout_value),  
-                       #  # Input - 4X4X128 | Output - 4X4X64 | RF=117
-
-                       nn.Conv2d(in_channels=32, out_channels=10, kernel_size=(1, 1), padding=0, bias=False)
-                       # Input - 4X4X32 | Output - 4X4X10 | RF=117
-
-                       )
-        # 4X4X10 | RF=121
-        self.gap = nn.Sequential(nn.AvgPool2d(kernel_size=4))
-        self.fc = nn.Linear(in_features = 10, out_features = 10)
-
-        # Input - 4X4X64 | Output - 1X1X64 
-        # self.fc1 = nn.Linear(128,10)
-        # self.fc2 = nn.Linear(64,10)
-
-    
     def forward(self, x):
-      
-      x = self.convblock_0(x)
-      x = self.convblock_1(x)
-      x = self.convblock_2(x)
-      x = self.convblock_3(x)
-      x = self.gap(x)
-      x = x.view(x.size(0), -1)
-      x = self.fc(x)
-      # x = self.fc1(x)
-      # x = self.fc2(x)     
-      x = x.view(-1, 10)
-      return F.log_softmax(x, dim=1)
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
 
-net = Net()
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def ResNet18():
+    return ResNet(BasicBlock, [2, 2, 2, 2])
+
+
+def ResNet34():
+    return ResNet(BasicBlock, [3, 4, 6, 3])
+
+
+def test():
+    net = ResNet18()
+    y = net(torch.randn(1, 3, 32, 32))
+    print(y.size())
+
+# test()
